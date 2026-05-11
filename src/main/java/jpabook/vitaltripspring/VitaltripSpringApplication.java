546882a83cefa2b564e4c906254e57570cccdbaa
eunwoo-levi <eunwoo1341@gmail.com>
2026-05-11 21:46:26 +0900

    git init

---
+++ src/main/java/jpabook/vitaltripspring/VitaltripSpringApplication.java
@@ -0,0 +1,13 @@
+package jpabook.vitaltripspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaltripSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitaltripSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/vitaltripspring/VitaltripSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.vitaltripspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaltripSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
